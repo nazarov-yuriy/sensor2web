f248bb88f019c97c492886306e37cf3719543a89
Yuliya Smirnova <yuliya.sm7@gmail.com>
2023-06-29 19:55:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, render_template, request
-import math
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 allowed_keys = {'angle', 'delta_x'}
 state = {
     'angle': 0,
     'delta_x': 0
 }
-
 
 @app.route('/')
 def index():
@@ -20,15 +20,20 @@ def get_state_():
     return jsonify(state)
 
 
+@socketio.on('connect')
+def test_connect(auth):
+    emit('system', {'data': 'Connected'})
+
 @app.route('/set_state', methods=['POST'])
-def get_state():
+def set_state():
     global state
     data = request.get_json()
     if not set(data.keys()).issubset(allowed_keys):
         return "Wrong data", 400
     state = data
+    socketio.emit('set_state', data)
     return "ok"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    socketio.run(app, debug=True, host="0.0.0.0", port=8080)
